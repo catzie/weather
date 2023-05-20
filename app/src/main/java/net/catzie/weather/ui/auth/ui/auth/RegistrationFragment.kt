20e09b63181a9e7a311cb285ff133419da3d43a5
Catzie M <i@catzie.net>
2023-05-20 19:57:37 +0800

    Create Registration fragment and layout

---
+++ app/src/main/java/net/catzie/weather/ui/auth/ui/auth/RegistrationFragment.kt
@@ -0,0 +1,32 @@
+package net.catzie.weather.ui.auth.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import net.catzie.weather.R
+
+class RegistrationFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = RegistrationFragment()
+    }
+
+    private lateinit var viewModel: AuthViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}

3baca6c3e789c7aeaef31cfeea198adfdad124e7
Catzie M <i@catzie.net>
2023-05-20 20:02:46 +0800

    Make AuthActivity.kt viewbinding-ready

--- app/src/main/java/net/catzie/weather/ui/auth/AuthActivity.kt
+++ app/src/main/java/net/catzie/weather/ui/auth/AuthActivity.kt
@@ -2,17 +2,18 @@ package net.catzie.weather.ui.auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import net.catzie.weather.R
+import net.catzie.weather.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, LoginFragment.newInstance())
-                .commitNow()
-        }
+
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }

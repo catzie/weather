578b15895857da612a6649f76891818becab628f
Catzie M <i@catzie.net>
2023-05-20 20:03:15 +0800

    Make MainActivity.kt viewbinding-ready

--- app/src/main/java/net/catzie/weather/ui/main/MainActivity.kt
+++ app/src/main/java/net/catzie/weather/ui/main/MainActivity.kt
@@ -2,11 +2,17 @@ package net.catzie.weather.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import net.catzie.weather.R
+import net.catzie.weather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }

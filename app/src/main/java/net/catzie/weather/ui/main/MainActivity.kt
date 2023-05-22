b175140aea006f2929745814de6aab022d6a4849
Catzie M <i@catzie.net>
2023-05-22 16:46:06 +0800

    Setup main act. to init. viewmodel

--- app/src/main/java/net/catzie/weather/ui/main/MainActivity.kt
+++ app/src/main/java/net/catzie/weather/ui/main/MainActivity.kt
@@ -1,11 +1,15 @@
 package net.catzie.weather.ui.main
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import net.catzie.weather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MainViewModel by viewModels { MainViewModel.Factory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -14,5 +18,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        viewModel.testCall()
     }
 }

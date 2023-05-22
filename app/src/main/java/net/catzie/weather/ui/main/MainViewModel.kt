9dbae2e4fa5a06968650763ae0165045b9fb24ad
Catzie M <i@catzie.net>
2023-05-22 15:30:38 +0800

    Create viewmodel livedata for weather result

--- app/src/main/java/net/catzie/weather/ui/main/MainViewModel.kt
+++ app/src/main/java/net/catzie/weather/ui/main/MainViewModel.kt
@@ -1,7 +1,15 @@
 package net.catzie.weather.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import net.catzie.weather.model.ApiResult
+import net.catzie.weather.model.WeatherResponse
 
 class MainViewModel : ViewModel() {
+
+    private val _weather = MutableLiveData<ApiResult<WeatherResponse>>()
+    val weather: LiveData<ApiResult<WeatherResponse>> = _weather
+
 
 }

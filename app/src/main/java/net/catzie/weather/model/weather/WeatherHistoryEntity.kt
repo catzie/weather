802ac2d44bb48fb220b51b9fe0d4f30fc78052f0
Catzie M <i@catzie.net>
2023-05-22 18:12:59 +0800

    Add entity properties

--- app/src/main/java/net/catzie/weather/model/weather/WeatherHistoryEntity.kt
+++ app/src/main/java/net/catzie/weather/model/weather/WeatherHistoryEntity.kt
@@ -6,12 +6,14 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "weather_history")
 data class WeatherHistoryEntity(
-    @PrimaryKey val id: Int,
-    @ColumnInfo(name = "weather") val weather: String,
+    @PrimaryKey(autoGenerate = true) val id: Int? = null,
+    @ColumnInfo(name = "weather_main") val weather_main: String,
+    @ColumnInfo(name = "weather_description") val weather_description: String,
+    @ColumnInfo(name = "weather_icon") val weather_icon: String,
     @ColumnInfo(name = "city") val city: String,
     @ColumnInfo(name = "country") val country: String,
     @ColumnInfo(name = "temp") val temp: Double,
-    @ColumnInfo(name = "sunrise_time") val sunriseTime: Int,
-    @ColumnInfo(name = "sunset_time") val sunsetTime: Int,
+    @ColumnInfo(name = "sunrise_time") val sunriseTime: Long,
+    @ColumnInfo(name = "sunset_time") val sunsetTime: Long,
 
     )

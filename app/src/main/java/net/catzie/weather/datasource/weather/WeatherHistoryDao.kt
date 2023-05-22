eb0164e6394f4583f41d5acaf5f7ea1d8e9cfed6
Catzie M <i@catzie.net>
2023-05-22 17:21:53 +0800

    Create WeatherHistoryDao

---
+++ app/src/main/java/net/catzie/weather/datasource/weather/WeatherHistoryDao.kt
@@ -0,0 +1,16 @@
+package net.catzie.weather.datasource.weather
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import net.catzie.weather.model.weather.WeatherHistoryEntity
+
+@Dao
+interface WeatherHistoryDao {
+
+    @Query("SELECT * FROM weather_history")
+    fun getAll(): List<WeatherHistoryEntity>
+
+    @Insert
+    fun insertAll(users: WeatherHistoryEntity)
+}

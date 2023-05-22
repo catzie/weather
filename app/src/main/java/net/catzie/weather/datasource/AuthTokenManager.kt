1f69ef58c385b71a3f8e7766cfb2434ac744d6c5
Catzie M <i@catzie.net>
2023-05-22 13:31:22 +0800

    Setup encryptedPreferences in AuthTokenManager.kt

--- app/src/main/java/net/catzie/weather/datasource/AuthTokenManager.kt
+++ app/src/main/java/net/catzie/weather/datasource/AuthTokenManager.kt
@@ -1,6 +1,26 @@
 package net.catzie.weather.datasource
 
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 
-class AuthTokenManager() {
+
+class AuthTokenManager(private val context: Context) {
+
+    private val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+    private val encryptedPreferences: SharedPreferences
+
+    init {
+
+        encryptedPreferences = EncryptedSharedPreferences.create(
+            "sharedprefsname",
+            masterKeyAlias,
+            context,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
+
 
 }

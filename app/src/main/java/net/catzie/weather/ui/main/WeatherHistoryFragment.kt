e8667e99816afe45b9cae1d3a45b7b28a28520ac
Catzie M <i@catzie.net>
2023-05-23 01:28:59 +0800

    Trigger fetch history from history fragment

--- app/src/main/java/net/catzie/weather/ui/main/WeatherHistoryFragment.kt
+++ app/src/main/java/net/catzie/weather/ui/main/WeatherHistoryFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import net.catzie.weather.R
+import net.catzie.weather.model.ApiResult
+import net.catzie.weather.model.weather.WeatherHistoryEntity
 import net.catzie.weather.ui.main.placeholder.PlaceholderContent
+import timber.log.Timber
 
 /**
  * A fragment representing a list of Items.
@@ -18,13 +22,20 @@ class WeatherHistoryFragment : Fragment() {
 
     private var columnCount = 1
 
+    val viewModel: MainViewModel by activityViewModels { MainViewModel.Factory }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         arguments?.let {
             columnCount = it.getInt(ARG_COLUMN_COUNT)
         }
+
+        // Let ViewModel know that we need weather history
+        viewModel.onWeatherHistoryFragmentCreated()
+
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,7 +53,18 @@ class WeatherHistoryFragment : Fragment() {
                 adapter = WeatherHistoryRecyclerViewAdapter(PlaceholderContent.ITEMS)
             }
         }
+
+        setUpObservers()
+
         return view
+    }
+
+    private fun setUpObservers() {
+        viewModel.history.observe(viewLifecycleOwner, ::displayWeatherHistory)
+    }
+
+    private fun displayWeatherHistory(apiResult: ApiResult<List<WeatherHistoryEntity>>?) {
+        Timber.e("historyfragment: $apiResult")
     }
 
     companion object {

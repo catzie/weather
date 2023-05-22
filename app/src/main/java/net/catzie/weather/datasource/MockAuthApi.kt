619251ccbb1394bb1efead552d37022e9a6c95a7
Catzie M <i@catzie.net>
2023-05-22 11:46:29 +0800

    Create test and class MockAuthApi for login/registration

---
+++ app/src/main/java/net/catzie/weather/datasource/MockAuthApi.kt
@@ -0,0 +1,51 @@
+package net.catzie.weather.datasource
+
+import net.catzie.social.model.auth.AuthLoginInput
+import net.catzie.social.model.auth.AuthRegisterInput
+import net.catzie.weather.model.AuthResponse
+import net.catzie.weather.ui.main.Utils.Randoms.getRandomId
+
+class MockAuthApi { // Non-mocked version will be ApiInterface
+
+
+    fun login(authLoginInput: AuthLoginInput): MockRetrofitAuthResponse {//todo on real auth: change MockRetrofitAuthResponse into retrofit2.Response<AuthResponse>
+        val mockRetrofitResponse = MockRetrofitAuthResponse()
+
+        with(authLoginInput) {
+
+            // Just check for filled values in this mock function
+            if (username.isBlank() || password.isBlank()) {
+                // fail
+                mockRetrofitResponse.code = 401
+                mockRetrofitResponse.body = null
+            } else {
+                //todo success
+                mockRetrofitResponse.code = 200
+                mockRetrofitResponse.body = AuthResponse(getRandomId().toString())
+            }
+        }
+
+        return mockRetrofitResponse
+    }
+
+    fun register(authRegisterInput: AuthRegisterInput): MockRetrofitAuthResponse {//todo on real auth: change MockRetrofitAuthResponse into retrofit2.Response<AuthResponse>
+        val mockRetrofitResponse = MockRetrofitAuthResponse()
+
+        with(authRegisterInput) {
+
+            // Just check for filled values in this mock function
+            if (username.isBlank() || password.isBlank() || firstname.isBlank() || lastname.isBlank()) {
+                // fail
+                mockRetrofitResponse.code = 401
+                mockRetrofitResponse.body = null
+            } else {
+                //todo success
+                mockRetrofitResponse.code = 200
+                mockRetrofitResponse.body = AuthResponse(getRandomId().toString())
+            }
+        }
+
+        return mockRetrofitResponse
+    }
+
+}
---
+++ app/src/test/java/net/catzie/weather/datasource/MockAuthApiTest.kt
@@ -0,0 +1,44 @@
+package net.catzie.weather.datasource
+
+import com.google.common.truth.Truth.assertThat
+import net.catzie.social.model.auth.AuthLoginInput
+import net.catzie.social.model.auth.AuthRegisterInput
+import org.junit.Before
+import org.junit.Test
+
+class MockAuthApiTest {
+
+    val SUT = MockAuthApi()
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun givenLoginFunction_whenProvidedCompleteInput_thenReturnCode200() {
+        val authInput = AuthLoginInput("myuser", "mypass")
+        val result = SUT.login(authInput)
+        assertThat(result.code).isEqualTo(200)
+    }
+
+    @Test
+    fun givenLoginFunction_whenProvidedNoInput_thenReturnNotCode200() {
+        val authInput = AuthLoginInput("", "   ")
+        val result = SUT.login(authInput)
+        assertThat(result.code).isNotEqualTo(200)
+    }
+
+    @Test
+    fun givenRegisterFunction_whenProvidedCompleteInput_thenReturnCode200() {
+        val authInput = AuthRegisterInput("myuser", "mypass", "fname", "lname")
+        val result = SUT.register(authInput)
+        assertThat(result.code).isEqualTo(200)
+    }
+
+    @Test
+    fun givenRegisterFunction_whenProvidedNoInput_thenReturnNotCode200() {
+        val authInput = AuthRegisterInput("", "  ", "  ", "")
+        val result = SUT.register(authInput)
+        assertThat(result.code).isNotEqualTo(200)
+    }
+}

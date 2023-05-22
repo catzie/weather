e5ce7f1fae2e61f8426fa2bd38a687cbf5cee2b7
Catzie M <i@catzie.net>
2023-05-22 12:12:48 +0800

    Add registration frag observers to navigate away on success.

--- app/src/main/java/net/catzie/weather/ui/auth/RegistrationFragment.kt
+++ app/src/main/java/net/catzie/weather/ui/auth/RegistrationFragment.kt
@@ -1,14 +1,19 @@
 package net.catzie.weather.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import net.catzie.weather.R
 import net.catzie.weather.databinding.FragmentRegistrationBinding
+import net.catzie.weather.model.ApiResult
+import net.catzie.weather.model.FakeAuthResponse
+import net.catzie.weather.ui.main.MainActivity
 
 class RegistrationFragment : Fragment() {
 
@@ -31,9 +36,52 @@ class RegistrationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setUpListeners()
+        setUpObservers()
+    }
+
+    private fun setUpListeners() {
         binding.btnLoginFromRegister.setOnClickListener {
             findNavController().navigate(R.id.action_registrationFragment_to_loginFragment2)
         }
+
+        binding.btnRegister.setOnClickListener {
+            viewModel.onClickRegister(
+                binding.etUsername.text.toString(),
+                binding.etPassword.text.toString(),
+                binding.etFirstname.text.toString(),
+                binding.etLastname.text.toString(),
+            )
+        }
     }
 
+    private fun setUpObservers() {
+        viewModel.registrationResult.observe(viewLifecycleOwner, ::displayRegistrationResult)
+    }
+
+    private fun displayRegistrationResult(apiResult: ApiResult<FakeAuthResponse>) {
+        when (apiResult) {
+
+            is ApiResult.Error -> {
+
+                // Display success message
+                val toastMessage = "Error: " + getString(apiResult.errorResId)
+                Toast.makeText(context, toastMessage, Toast.LENGTH_LONG).show()
+            }
+
+            is ApiResult.Success -> {
+
+                // Display success message
+                val toastMessage = getString(R.string.registration_res_success)
+                Toast.makeText(context, toastMessage, Toast.LENGTH_LONG).show()
+
+                // Launch MainActivity
+                val intent = Intent(activity, MainActivity::class.java)
+                activity?.startActivity(intent)
+
+                // Exit AuthActivity
+                activity?.finish()
+            }
+        }
+    }
 }

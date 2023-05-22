17e3a4b57362d3f684be5c5f6f29c86efd4dbd9b
Catzie M <i@catzie.net>
2023-05-22 13:53:34 +0800

    Modify viewmodel to navigate to main screen if previously loggedin/registered

--- app/src/main/java/net/catzie/weather/ui/auth/AuthViewModel.kt
+++ app/src/main/java/net/catzie/weather/ui/auth/AuthViewModel.kt
@@ -5,19 +5,33 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import kotlinx.coroutines.launch
 import net.catzie.social.model.auth.AuthLoginInput
 import net.catzie.social.model.auth.AuthRegisterInput
+import net.catzie.weather.MyApplication
 import net.catzie.weather.R
 import net.catzie.weather.datasource.AuthRepository
+import net.catzie.weather.datasource.AuthSessionManager
 import net.catzie.weather.datasource.MockAuthApi
 import net.catzie.weather.model.ApiResult
 import net.catzie.weather.model.FakeAuthResponse
 
-class AuthViewModel(private val authRepository: AuthRepository) : ViewModel() {
+class AuthViewModel(
+    private val authRepository: AuthRepository,
+    private val authSessionManager: AuthSessionManager
+) : ViewModel() {
 
     private val _loginResult = MutableLiveData<ApiResult<FakeAuthResponse>>()
     val loginResult: LiveData<ApiResult<FakeAuthResponse>> = _loginResult
 
     private val _registrationResult = MutableLiveData<ApiResult<FakeAuthResponse>>()
     val registrationResult: LiveData<ApiResult<FakeAuthResponse>> = _registrationResult
+
+    init {
+
+        // if user is not empty, signalUiToLogUserIn
+        val userId = authSessionManager.loadUserId()
+        if (!userId.isNullOrBlank()) {
+            signalUiToLogUserIn(FakeAuthResponse(userId))
+        }
+    }
 
     fun onClickLogin(username: String, password: String) {
 
@@ -30,7 +44,8 @@ class AuthViewModel(private val authRepository: AuthRepository) : ViewModel() {
             if (result.code() == 200) {
                 result.body()?.let { auth ->
 
-                    // todo Save token or user info
+                    // Save token or user info
+                    auth.userId.let { authSessionManager.saveUserId(it) }
 
                     //  Give signal to log user in on success
                     signalUiToLogUserIn(auth)
@@ -53,7 +68,9 @@ class AuthViewModel(private val authRepository: AuthRepository) : ViewModel() {
             val result = authRepository.register(authRegInput)
             if (result.code() == 200) {
                 result.body()?.let { auth ->
-                    // todo Save token or user info
+
+                    // Save token or user info
+                    auth.userId.let { authSessionManager.saveUserId(it) }
 
                     //  Give signal to log user in on success
                     signalUiToRegUserIn(auth)
@@ -63,7 +80,6 @@ class AuthViewModel(private val authRepository: AuthRepository) : ViewModel() {
                 _registrationResult.value = ApiResult.Error(R.string.registration_res_failed)
             }
         }
-
     }
 
     private fun signalUiToLogUserIn(auth: FakeAuthResponse) {
@@ -82,7 +98,8 @@ class AuthViewModel(private val authRepository: AuthRepository) : ViewModel() {
                     val application =
                         checkNotNull(extras[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY])
                     return AuthViewModel(
-                        AuthRepository(MockAuthApi())
+                        AuthRepository(MockAuthApi()),
+                        (application as MyApplication).authSessionManager
                     ) as T
                 }
                 throw IllegalArgumentException("Unknown ViewModel class")

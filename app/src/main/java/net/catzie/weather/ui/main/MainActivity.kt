99ce675dc2e9f33407a2b1239c0aa085ff8fce4d
Catzie M <i@catzie.net>
2023-05-22 20:01:07 +0800

    Setup tabbed layout from main activity

--- app/src/main/java/net/catzie/weather/ui/main/MainActivity.kt
+++ app/src/main/java/net/catzie/weather/ui/main/MainActivity.kt
@@ -3,6 +3,8 @@ package net.catzie.weather.ui.main
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayoutMediator
+import net.catzie.weather.R
 import net.catzie.weather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +21,24 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        //todo Remove once you make a necessary call to the viewmodel
         viewModel.testCall()
+
+        setUpTabbedLayout()
+    }
+
+    private fun setUpTabbedLayout() {
+
+        //Setup viewpager
+        binding.viewPager.adapter = ViewPagerAdapter(this)
+
+        //Setup tablayout
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> getString(R.string.tab_current)
+                1 -> getString(R.string.tab_history)
+                else -> throw IllegalArgumentException("Invalid position: $position")
+            }
+        }.attach()
     }
 }

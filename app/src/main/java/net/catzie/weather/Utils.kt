c6ac34ed227b6fafd7fffc65a54bb3280b86127e
Catzie M <i@catzie.net>
2023-05-23 02:31:22 +0800

    Set values for weather history data

--- app/src/main/java/net/catzie/weather/Utils.kt
+++ app/src/main/java/net/catzie/weather/Utils.kt
@@ -1,5 +1,7 @@
 package net.catzie.weather
 
+import java.text.SimpleDateFormat
+import java.util.*
 import kotlin.math.abs
 
 object Utils {
@@ -7,6 +9,14 @@ object Utils {
     object Randoms {
         fun getRandomId(): Int {
             return abs((0..9999).random())
+        }
+    }
+
+    object Format {
+        fun formatDateTime(timeInMillis: Long): String {
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+            val dateTime = Date(timeInMillis)
+            return dateFormat.format(dateTime)
         }
     }
 }
--- app/src/main/java/net/catzie/weather/ui/main/WeatherHistoryRecyclerViewAdapter.kt
+++ app/src/main/java/net/catzie/weather/ui/main/WeatherHistoryRecyclerViewAdapter.kt
@@ -2,47 +2,57 @@ package net.catzie.weather.ui.main
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import net.catzie.weather.Utils.Format.formatDateTime
 import net.catzie.weather.databinding.FragmentWeatherHistoryListItemBinding
-import net.catzie.weather.ui.main.placeholder.PlaceholderContent.PlaceholderItem
+import net.catzie.weather.model.weather.WeatherHistoryEntity
+import java.util.*
 
 /**
  * [RecyclerView.Adapter] that can display a [PlaceholderItem].
  * TODO: Replace the implementation with code for your data type.
  */
 class WeatherHistoryRecyclerViewAdapter(
-    private val values: List<PlaceholderItem>
+    private var dataSet: List<WeatherHistoryEntity>
 ) : RecyclerView.Adapter<WeatherHistoryRecyclerViewAdapter.ViewHolder>() {
+
+    private lateinit var binding: FragmentWeatherHistoryListItemBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
-        return ViewHolder(
-            FragmentWeatherHistoryListItemBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
+        binding = FragmentWeatherHistoryListItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
         )
+
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = values[position]
-        holder.idView.text = item.id
-        holder.contentView.text = item.content
+        val item = dataSet[position]
+
+        binding.weatherMain.text = item.weather_main
+        binding.weatherDescription.text = item.weather_description
+        binding.weatherIcon.text = item.weather_icon
+        binding.city.text = item.city
+        binding.country.text = item.country
+        binding.temp.text = item.temp.toString()
+        binding.sunriseTime.text = formatDateTime(item.sunriseTime)
+        binding.sunsetTime.text = formatDateTime(item.sunsetTime)
+
+
     }
 
-    override fun getItemCount(): Int = values.size
+    override fun getItemCount(): Int = dataSet.size
+
+    fun updateData(updatedData: List<WeatherHistoryEntity>) {
+        dataSet = updatedData
+        notifyDataSetChanged()
+    }
 
     inner class ViewHolder(binding: FragmentWeatherHistoryListItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
-
-        val idView: TextView = binding.itemNumber
-        val contentView: TextView = binding.content
-
-        override fun toString(): String {
-            return super.toString() + " '" + contentView.text + "'"
-        }
     }
 
 }

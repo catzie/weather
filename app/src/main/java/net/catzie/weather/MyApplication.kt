6975c930829303625289c23cae73f1c6e0257719
Catzie M <i@catzie.net>
2023-05-22 13:51:07 +0800

    Fix MyApplication.kt usage + add auth manager instance

--- app/src/main/java/net/catzie/weather/MyApplication.kt
+++ app/src/main/java/net/catzie/weather/MyApplication.kt
@@ -1,7 +1,12 @@
+package net.catzie.weather
+
 import android.app.Application
+import net.catzie.weather.datasource.AuthSessionManager
 import timber.log.Timber
 
 class MyApplication : Application() {
+
+    val authSessionManager by lazy { AuthSessionManager(this) }
 
     override fun onCreate() {
         super.onCreate()

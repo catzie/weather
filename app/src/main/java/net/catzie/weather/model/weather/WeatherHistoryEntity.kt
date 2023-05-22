ad7035be0470ba33b53a9334827297c51fc92607
Catzie M <i@catzie.net>
2023-05-22 18:12:52 +0800

    Define db entity table name

--- app/src/main/java/net/catzie/weather/model/weather/WeatherHistoryEntity.kt
+++ app/src/main/java/net/catzie/weather/model/weather/WeatherHistoryEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "weather_history")
 data class WeatherHistoryEntity(
     @PrimaryKey val id: Int,
     @ColumnInfo(name = "weather") val weather: String,
